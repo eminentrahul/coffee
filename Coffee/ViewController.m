bba96911e51d9650d063f38fb0dbdceacf5d9fcd
eminentrahul <eminentrahul@rocketmail.com>
2017-12-23 22:45:11 +0530

    Initial commit

--- Coffee/ViewController.m
+++ Coffee/ViewController.m
@@ -26,4 +26,17 @@
 }
 
 
+- (IBAction)calculateButtonPressed:(id)sender {
+    
+    //NSLog(@"Calculate button Pressed!");
+    
+    float water = [[self.waterTextField text] floatValue];
+    float ratio = [[self.ratioTextField text] floatValue];
+    
+    float coffee = water / ratio;
+    
+    NSString *coffeeText = [NSString stringWithFormat: @"%.3f", coffee];
+    
+    self.coffeeTextField.text = coffeeText;
+}
 @end
